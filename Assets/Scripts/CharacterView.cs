c83e20fa8acbaa3056ea256b90fe673f839a4466
Fabrizio <fabriziobarbieri@hotmail.com>
2021-10-31 13:19:47 -0300

    SimpleMenuFunctions

---
+++ Assets/Scripts/CharacterView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CharacterView : MonoBehaviourPun
+{
+    [SerializeField] TextMesh _nickNamePrefab;
+    TextMesh _nickname;
+    Camera _camera;
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+    private void Start()
+    {
+        _nickname = Instantiate<TextMesh>(_nickNamePrefab);
+    }
+}
--- Assets/Scripts/NetManager.cs
+++ Assets/Scripts/NetManager.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Realtime;
 using Photon.Pun;
+using UnityEngine.UI;
 
 public class NetManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] InputField _nickNameInput;
+    [SerializeField] Button button;
     private void Start()
     {
+        button.interactable = false;
         PhotonNetwork.ConnectUsingSettings();
     }
     public override void OnConnectedToMaster()
@@ -16,12 +20,27 @@ public class NetManager : MonoBehaviourPunCallbacks
     }
     public override void OnJoinedLobby()
     {
-        RoomOptions options = new RoomOptions();
-        options.MaxPlayers = 6;
-        PhotonNetwork.JoinOrCreateRoom("Room", options, TypedLobby.Default);
+        button.interactable = true;
     }
     public override void OnJoinedRoom()
     {
         PhotonNetwork.LoadLevel("Gameplay");
+    }
+    public void ConnectRoom() 
+    {
+        if (string.IsNullOrEmpty(_nickNameInput.text) || string.IsNullOrWhiteSpace(_nickNameInput.text)) return;
+        PhotonNetwork.NickName = _nickNameInput.text;
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 6;
+        PhotonNetwork.JoinOrCreateRoom("Room", options, TypedLobby.Default);
+        button.interactable = false;
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        button.interactable = true;
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        button.interactable = true;
     }
 }
